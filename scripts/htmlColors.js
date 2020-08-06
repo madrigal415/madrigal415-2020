3fa3d527f14a589aec9e59d0b995baced1b145cd
madrigal415 <dsmadrigal@gmail.com>
2020-08-06 16:29:04 -0700

    updating header color

--- scripts/htmlColors.js
+++ scripts/htmlColors.js
@@ -1,4 +1,4 @@
-'use strict;'
+'use strict';
 
 const colors = ['black', 'dimgray', 'slategrey', 'lightslategrey', 'navy', 'midnightblue', 'blue', 'steelblue', 'slateblue', 'aquamarine', 'skyblue', 'teal', 'turquoise', 'lightseagreen', 'mediumaquamarine', 'darkseagreen', 'darkturquoise', 'indigo', 'purple', 'darkviolet', 'blueviolet', 'violet', 'fuchsia', 'lavender', 'plum', 'pink', 'palevioletred', 'deeppink', 'lightcyan', 'lightsteelblue', 'cadetblue', 'chartreuse', 'cornflowerblue', 'cyan', 'darkcyan', 'darkkhaki', 'darkolivegreen', 'darkred', 'darksalmon', 'deepskyblue', 'forestgreen', 'brown', 'darkorange', 'mediumvioletred', 'magenta', 'darksalmon', 'tomato', 'salmon', 'lightsalmon', 'dimgray', 'firebrick', 'red', 'indianred', 'crimson', 'green', 'lawngreen', 'greenyellow', 'lightgreen', 'limegreen', 'lime', 'mintcream', 'navajowhite', 'papayawhip', 'goldenrod', 'gold', 'peru', 'lemonchiffon', 'lightyellow', 'lightgoldenrodyellow', 'whitesmoke', 'azure', 'mocassin', 'beige', 'antiquewhite', 'floralwhite', 'wheat', 'honeydew', 'ghostwhite', 'cornsilk', 'snow', 'white'];
 
@@ -6,8 +6,9 @@ const container = document.querySelector("#colorContainer");
 
 colors.forEach(function (x) {
     console.log(colorNameToHex(x));
+    console.log(hexToRGB('#7fffd4'));
 
-    container.insertAdjacentHTML("afterbegin", `<div class="color" style="background:${x};">${x}<span class="hex">${colorNameToHex(x)}<span></div>`);
+    container.insertAdjacentHTML("afterbegin", `<div class="color" style="background:${x};">${x}<span class="hex">${colorNameToHex(x)}</span><span class="rgb">${hexToRGB(colorNameToHex(x))}</span><span class="hsl">${RGBToHSL(hexToRGB(colorNameToHex(x)))}</span></div>`);
 });
 
 
@@ -44,3 +45,85 @@ function colorNameToHex(colour) {
 
     return false;
 }
+
+
+function hexToRGB(h) {
+    let r = 0, g = 0, b = 0;
+
+    // 3 digits
+    if (h.length == 4) {
+        r = "0x" + h[1] + h[1];
+        g = "0x" + h[2] + h[2];
+        b = "0x" + h[3] + h[3];
+
+        // 6 digits
+    } else if (h.length == 7) {
+        r = "0x" + h[1] + h[2];
+        g = "0x" + h[3] + h[4];
+        b = "0x" + h[5] + h[6];
+    }
+
+    return "rgb(" + +r + "," + +g + "," + +b + ")";
+}
+
+function RGBToHSL(rgb) {
+
+    console.log(rgb);
+    let sep = rgb.indexOf(",") > -1 ? "," : " ";
+    rgb = rgb.substr(4).split(")")[0].split(sep);
+
+    for (let R in rgb) {
+        let r = rgb[R];
+        if (r.indexOf("%") > -1)
+            rgb[R] = Math.round(r.substr(0, r.length - 1) / 100 * 255);
+    }
+
+    // Make r, g, and b fractions of 1
+    let r = rgb[0] / 255,
+        g = rgb[1] / 255,
+        b = rgb[2] / 255;
+
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r, g, b),
+        cmax = Math.max(r, g, b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+
+    // Calculate hue
+    // No difference
+    if (delta == 0)
+        h = 0;
+    // Red is max
+    else if (cmax == r)
+        h = ((g - b) / delta) % 6;
+    // Green is max
+    else if (cmax == g)
+        h = (b - r) / delta + 2;
+    // Blue is max
+    else
+        h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    // Make negative hues positive behind 360°
+    if (h < 0)
+        h += 360;
+
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    return "hsl(" + h + "," + s + "%," + l + "%)";
+}
+
+console.log('here ', RGBToHSL('rgb(120,50,234)'));
+
+console.log('LENGTH ', colors.length);
